ff07a136191f0853111a86563c3138b7711dd203
Bas Voss <voss.bas@gmail.com>
2020-07-14 21:11:34 +0200

    try on connection

--- app/db.js
+++ app/db.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const db = null;
+try{ 
+    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@pawpatrolshop.j082l.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@pawpatrolshop.j082l.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('connected to db');
-});
+    const db = mongoose.connection;
+    
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+        console.log('connected to db');
+    });
+    
+} catch (err) {
+    res.status(500).json({ message: err.toString() });
+}
 
 module.exports = db;
