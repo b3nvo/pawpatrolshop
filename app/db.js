7487198d3db2f993884fd24c1802726fdd6d2d46
Bas Voss <voss.bas@gmail.com>
2020-07-14 21:21:04 +0200

    try on connection

--- app/db.js
+++ app/db.js
@@ -4,7 +4,7 @@ try{
     mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@pawpatrolshop.j082l.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
     
     const db = mongoose.connection;
-    console.log('connection: ', db);
+    
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', () => {
         console.log('connected to db');
