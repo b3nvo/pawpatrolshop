e8fceeeda40928e36008622d1c5d39dec53e93e0
Bas Voss <voss.bas@gmail.com>
2020-07-29 15:50:04 +0200

    possible not found fix

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Admin.css';
 
-function Admin() {
-    return(
-        <h1>Admin page</h1>
-    )
+class Admin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {}
+        };
+    }
+
+    render() {
+        return(
+            <h1>Admin page</h1>
+        )
+    }
 }
 
 export default Admin;
