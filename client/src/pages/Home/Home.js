2e07ebe4ec7306b32a99c1679b577f5171747b5f
Bas Voss <voss.bas@gmail.com>
2020-08-02 13:24:46 +0200

    edit latest products

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,39 +1,60 @@
 import React from "react";
 import Latest from "./Latest";
 import CarouselProducts from "./Carousel.js";
+import { Container, Row } from 'react-bootstrap';
 import { withRouter } from "react-router-dom";
 import "./Home.css";
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      payload: this.props.payload,
-      token: this.props.token
+    constructor(props) {
+        super(props);
+        this.state = {
+            payload: this.props.payload,
+            token: this.props.token,
+            products: []
+        };
+
+        if (this.props.location.state) {
+            this.setState({
+                payload: this.props.location.state.payload,
+                token: this.props.location.state.token
+            });
+        }
+
+        this.fetchProducts = this.fetchProducts.bind(this);
     }
 
-    if (this.props.location.state) { 
-      this.setState({payload: this.props.location.state.payload, token: this.props.location.state.token});
+    async fetchProducts() {
+        const response = await fetch("/api/products/latest");
+        const json = await response.json();
+        this.setState({ products: json.data });
     }
-  }
 
-  render() {
-    return (
-      <div className="home">
-        <div className="carousel">
-          <CarouselProducts />
-        </div>
+    render() {
+        const { products } = this.state;
 
-        <div className="products">
-          <Latest />
-        </div>
+        return (
+            <div className="home">
+                <div className="carousel">
+                    <CarouselProducts />
+                </div>
 
-        <div className="footer">
-        <h1> footer here </h1>
-      </div>
-      </div>
-    );
-  }
+                {/* creating grid view */}
+                <div className="products">
+                    <h2 className="text-center">Latest products</h2>
+                    <Container className="w-100">
+                        <Row className="w-100">
+                            <Latest fetchProducts={this.fetchProducts} products={products} />
+                        </Row>
+                    </Container>
+                </div>
+
+                <div className="footer">
+                    <h1> footer here </h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withRouter(Home);
--- client/src/pages/Home/Latest.js
+++ client/src/pages/Home/Latest.js
@@ -1,42 +1,31 @@
 import React from "react";
 import "./Latest.css";
-import {Card} from 'react-bootstrap';
-
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 class Latest extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { products: [] };
+    async componentDidMount() {
+        console.log("hello");
 
-  }
+        this.props.fetchProducts();
+    }
 
-  async componentDidMount() {
-    console.log('hello')
-    const response = await fetch("/api/products/latest");
-    const json = await response.json();
-    this.setState({ products: json.data});
-  }
-
-  render() {
-    return (
-      <div className="producs">
-        <h1> Latest products </h1>
-        <div className="items">
-          {this.state.products.map(el => (
-            <Card className="card" key={el._id}>
-              <Card.Img variant="top" src={el.imagePath} />
-              <Card.Body>
-                <Card.Title>{el.name}</Card.Title>
-                <Card.Text>
-                  {el.description}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <>
+                {this.props.products.map((el) => (
+                    <Col xs="4" className="col-card">
+                        <Card className="card" key={el._id}>
+                            <Card.Img variant="top" src={el.imagePath} />
+                            <Card.Body>
+                                <Card.Title>{el.name}</Card.Title>
+                                <Card.Text>{el.description} </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </>
+        );
+    }
 }
 
 export default Latest;
