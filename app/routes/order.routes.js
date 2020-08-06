54987e8471797d9e9ed40baa8d660d628cb8afaa
Bas Voss <Bas Voss>
2020-08-06 15:57:52 +0200

    order routes

---
+++ app/routes/order.routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const account = require("../controllers/user.controller");
+const controller = require("../controllers/order.controller");
+
+// POST
+router.post("/", account.checkUser, controller.postOrder);
+
+// GET
+router.get("/", account.checkUser, controller.getOrders);
+router.get(
+  "/:orderID",
+  account.checkUser,
+  account.checkOrderUser,
+  controller.getOrder
+);
+
+// UPDATE
+router.put("/:orderID", account.checkUser, controller.updateOrderById);
+
+// DELETE
+router.delete("/:orderID", account.checkUser, controller.DeleteOrderByID);
+
+module.exports = router;
